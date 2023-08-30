94efa8b67b5baacd8dda270888ef3122e77bea3f
tanu <dastanushri402@gmail.com>
2023-08-30 18:44:36 +0600

    again update readme file

--- src/Redux/selectors.js
+++ src/Redux/selectors.js
@@ -1,4 +1,4 @@
-// selectors.js
+
 export const selectContactById = (state, contactId) => {
    // Using the `find` method to search for a contact in the `state.contacts` array whose `id` matches the provided `contactId`.
   // The `find` method returns the first element that satisfies the condition, or `undefined` if no element is found.
